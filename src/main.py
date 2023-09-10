65ab2b0b01be10d5aa732e40601028d43b9cd7b9
jackvoorham <60979627+jackvoorham@users.noreply.github.com>
2023-09-10 11:35:34 +0200

    feat: add arguments parsing

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import logging
+import argparse
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
@@ -6,3 +7,20 @@ logging.basicConfig(
 
 if __name__ == "__main__":
     logging.info("Starting the application")
+
+    logging.info("Parsing the arguments")
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-dp1", help="Path to client dataset", type=str, required=True)
+    parser.add_argument(
+        "-dp2", help="Path to financial dataset", type=str, required=True
+    )
+    parser.add_argument(
+        "-c", help="List of countries to filter on", type=str, nargs="+", required=True
+    )
+
+    args = parser.parse_args()
+
+    logging.info("Client data path: %s", args.dp1)
+    logging.info("Financial data path: %s", args.dp2)
+    logging.info("Countries: %s", args.c)
