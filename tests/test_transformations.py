41ea298bfe292c52776918dab5a9f011c53b307f
jackvoorham <60979627+jackvoorham@users.noreply.github.com>
2023-09-11 12:46:31 +0200

    feat: add client sanitizing test

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -22,3 +22,20 @@ def test_filter_by_countries():
     assert result.shape == (2, 2)
     assert "France" not in result["country"].values
     assert "Spain" not in result["country"].values
+
+
+def test_sanitize_client_data():
+    test_data = pd.DataFrame(
+        {
+            "id": [1, 2, 3],
+            "first_name": ["John", "Jane", "Mary"],
+            "last_name": ["Doe", "Doe", "Smith"],
+        }
+    )
+
+    result = sanitize_client_data(test_data)
+
+    # Assert properties
+    assert result.shape == (3, 1)
+    assert "first_name" not in result.columns
+    assert "last_name" not in result.columns
